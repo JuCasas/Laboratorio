fedeeb7f562b715145575aa49a7fab362f8653c1
Jeferson Miranda Guillermo <jeferson.miranda@pucp.edu.pe>
2020-10-21 16:22:35 -0500

    Final Jeferson

--- LAB6Soft/frmLogin.Designer.cs
+++ LAB6Soft/frmLogin.Designer.cs
@@ -150,10 +150,9 @@
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.DarkSlateBlue;
             this.pictureBox1.Image = global::LAB6Soft.Properties.Resources._3961494681_a4fca1a1_5ef9_4440_93f7_764ea249e4f6;
-            this.pictureBox1.Location = new System.Drawing.Point(467, 87);
+            this.pictureBox1.Location = new System.Drawing.Point(574, 96);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(300, 112);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.Size = new System.Drawing.Size(118, 122);
             this.pictureBox1.TabIndex = 12;
             this.pictureBox1.TabStop = false;
             // 
