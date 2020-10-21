1e7df4d0a86d09473c9fef05f5eb26b41e0135ab
Jeferson Miranda Guillermo <jeferson.miranda@pucp.edu.pe>
2020-10-21 12:07:00 -0500

    Agregué formularios para buscar proveedor y usuario.

---
+++ LAB6Soft/frmBuscarProveedor.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace LAB6Soft {
+    partial class frmBuscarProveedor {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.dgvBuscarProv = new System.Windows.Forms.DataGridView();
+            this.btnSeleccionarProv = new System.Windows.Forms.Button();
+            this.txtBuscarProv = new System.Windows.Forms.TextBox();
+            this.lblBuscarProv = new System.Windows.Forms.Label();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nombres = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ApellidoPaterno = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Rol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Area = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBuscarProv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvBuscarProv
+            // 
+            this.dgvBuscarProv.AllowUserToAddRows = false;
+            this.dgvBuscarProv.AllowUserToDeleteRows = false;
+            this.dgvBuscarProv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvBuscarProv.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.Nombres,
+            this.ApellidoPaterno,
+            this.Rol,
+            this.Area});
+            this.dgvBuscarProv.Location = new System.Drawing.Point(23, 94);
+            this.dgvBuscarProv.Name = "dgvBuscarProv";
+            this.dgvBuscarProv.ReadOnly = true;
+            this.dgvBuscarProv.RowHeadersWidth = 51;
+            this.dgvBuscarProv.RowTemplate.Height = 24;
+            this.dgvBuscarProv.Size = new System.Drawing.Size(681, 362);
+            this.dgvBuscarProv.TabIndex = 7;
+            // 
+            // btnSeleccionarProv
+            // 
+            this.btnSeleccionarProv.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSeleccionarProv.Location = new System.Drawing.Point(584, 35);
+            this.btnSeleccionarProv.Name = "btnSeleccionarProv";
+            this.btnSeleccionarProv.Size = new System.Drawing.Size(120, 28);
+            this.btnSeleccionarProv.TabIndex = 6;
+            this.btnSeleccionarProv.Text = "Seleccionar";
+            this.btnSeleccionarProv.UseVisualStyleBackColor = true;
+            // 
+            // txtBuscarProv
+            // 
+            this.txtBuscarProv.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBuscarProv.Location = new System.Drawing.Point(159, 35);
+            this.txtBuscarProv.Name = "txtBuscarProv";
+            this.txtBuscarProv.Size = new System.Drawing.Size(297, 28);
+            this.txtBuscarProv.TabIndex = 5;
+            // 
+            // lblBuscarProv
+            // 
+            this.lblBuscarProv.AutoSize = true;
+            this.lblBuscarProv.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBuscarProv.Location = new System.Drawing.Point(19, 38);
+            this.lblBuscarProv.Name = "lblBuscarProv";
+            this.lblBuscarProv.Size = new System.Drawing.Size(123, 21);
+            this.lblBuscarProv.TabIndex = 4;
+            this.lblBuscarProv.Text = "Buscar (RUC):";
+            // 
+            // ID
+            // 
+            this.ID.HeaderText = "ID";
+            this.ID.MinimumWidth = 6;
+            this.ID.Name = "ID";
+            this.ID.ReadOnly = true;
+            this.ID.Width = 30;
+            // 
+            // Nombres
+            // 
+            this.Nombres.HeaderText = "Razón Social";
+            this.Nombres.MinimumWidth = 6;
+            this.Nombres.Name = "Nombres";
+            this.Nombres.ReadOnly = true;
+            this.Nombres.Width = 200;
+            // 
+            // ApellidoPaterno
+            // 
+            this.ApellidoPaterno.HeaderText = "RUC";
+            this.ApellidoPaterno.MinimumWidth = 6;
+            this.ApellidoPaterno.Name = "ApellidoPaterno";
+            this.ApellidoPaterno.ReadOnly = true;
+            this.ApellidoPaterno.Width = 120;
+            // 
+            // Rol
+            // 
+            this.Rol.HeaderText = "Dirección";
+            this.Rol.MinimumWidth = 6;
+            this.Rol.Name = "Rol";
+            this.Rol.ReadOnly = true;
+            this.Rol.Width = 200;
+            // 
+            // Area
+            // 
+            this.Area.HeaderText = "Detracción";
+            this.Area.MinimumWidth = 6;
+            this.Area.Name = "Area";
+            this.Area.ReadOnly = true;
+            this.Area.Width = 80;
+            // 
+            // frmBuscarProveedor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(722, 491);
+            this.Controls.Add(this.dgvBuscarProv);
+            this.Controls.Add(this.btnSeleccionarProv);
+            this.Controls.Add(this.txtBuscarProv);
+            this.Controls.Add(this.lblBuscarProv);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmBuscarProveedor";
+            this.Text = "Formulario para Buscar un Proveedor";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBuscarProv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvBuscarProv;
+        private System.Windows.Forms.Button btnSeleccionarProv;
+        private System.Windows.Forms.TextBox txtBuscarProv;
+        private System.Windows.Forms.Label lblBuscarProv;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombres;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ApellidoPaterno;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Rol;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Area;
+    }
+}
--- LAB6Soft/frmLogin.Designer.cs
+++ LAB6Soft/frmLogin.Designer.cs
@@ -35,28 +35,26 @@
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox5 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox4 = new System.Windows.Forms.PictureBox();
-            this.pictureBox6 = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(25)))), ((int)(((byte)(74)))));
+            this.label1.BackColor = System.Drawing.Color.DarkSlateBlue;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.SlateGray;
-            this.label1.Location = new System.Drawing.Point(592, 192);
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(528, 252);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(93, 25);
             this.label1.TabIndex = 1;
@@ -66,8 +64,8 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.SlateGray;
-            this.label2.Location = new System.Drawing.Point(592, 293);
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(528, 353);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(131, 25);
             this.label2.TabIndex = 2;
@@ -80,7 +78,7 @@
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox1.ForeColor = System.Drawing.SystemColors.Window;
-            this.textBox1.Location = new System.Drawing.Point(597, 330);
+            this.textBox1.Location = new System.Drawing.Point(533, 390);
             this.textBox1.Name = "textBox1";
             this.textBox1.PasswordChar = '●';
             this.textBox1.Size = new System.Drawing.Size(254, 38);
@@ -92,26 +90,26 @@
             this.button1.BackColor = System.Drawing.Color.SlateGray;
             this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(41)))), ((int)(((byte)(86)))));
-            this.button1.Location = new System.Drawing.Point(669, 416);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(563, 504);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(126, 41);
+            this.button1.Size = new System.Drawing.Size(177, 41);
             this.button1.TabIndex = 5;
-            this.button1.Text = "Iniciar Sesion";
+            this.button1.Text = "Iniciar Sesión";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // linkLabel1
             // 
             this.linkLabel1.AutoSize = true;
-            this.linkLabel1.LinkColor = System.Drawing.Color.White;
-            this.linkLabel1.Location = new System.Drawing.Point(823, 543);
+            this.linkLabel1.LinkColor = System.Drawing.Color.DarkGray;
+            this.linkLabel1.Location = new System.Drawing.Point(530, 431);
             this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(123, 17);
+            this.linkLabel1.Size = new System.Drawing.Size(141, 17);
             this.linkLabel1.TabIndex = 6;
             this.linkLabel1.TabStop = true;
-            this.linkLabel1.Text = "Olvide contraseña";
+            this.linkLabel1.Text = "Olvidé mi contraseña";
             this.linkLabel1.VisitedLinkColor = System.Drawing.Color.White;
             this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
             // 
@@ -121,7 +119,7 @@
             this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox2.ForeColor = System.Drawing.SystemColors.Window;
-            this.textBox2.Location = new System.Drawing.Point(597, 232);
+            this.textBox2.Location = new System.Drawing.Point(533, 292);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(254, 38);
             this.textBox2.TabIndex = 8;
@@ -136,46 +134,6 @@
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
             // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::LAB6Soft.Properties.Resources._3961494681_a4fca1a1_5ef9_4440_93f7_764ea249e4f6;
-            this.pictureBox1.Location = new System.Drawing.Point(655, 57);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(300, 112);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
-            this.pictureBox1.TabIndex = 12;
-            this.pictureBox1.TabStop = false;
-            // 
-            // pictureBox5
-            // 
-            this.pictureBox5.Image = global::LAB6Soft.Properties.Resources.kisspng_accounting_accountant_computer_icons_bookkeeping_f_finance_5abc92d44cb875_8221489015223077963143;
-            this.pictureBox5.Location = new System.Drawing.Point(128, 131);
-            this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(300, 300);
-            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox5.TabIndex = 11;
-            this.pictureBox5.TabStop = false;
-            // 
-            // pictureBox3
-            // 
-            this.pictureBox3.Image = global::LAB6Soft.Properties.Resources.background_text;
-            this.pictureBox3.Location = new System.Drawing.Point(597, 232);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(254, 38);
-            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox3.TabIndex = 7;
-            this.pictureBox3.TabStop = false;
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.Image = global::LAB6Soft.Properties.Resources.background_text;
-            this.pictureBox2.Location = new System.Drawing.Point(597, 330);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(254, 38);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox2.TabIndex = 7;
-            this.pictureBox2.TabStop = false;
-            // 
             // pictureBox4
             // 
             this.pictureBox4.Cursor = System.Windows.Forms.Cursors.Hand;
@@ -188,21 +146,53 @@
             this.pictureBox4.TabStop = false;
             this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
             // 
-            // pictureBox6
+            // pictureBox1
             // 
-            this.pictureBox6.Location = new System.Drawing.Point(816, 57);
-            this.pictureBox6.Name = "pictureBox6";
-            this.pictureBox6.Size = new System.Drawing.Size(186, 112);
-            this.pictureBox6.TabIndex = 13;
-            this.pictureBox6.TabStop = false;
+            this.pictureBox1.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.pictureBox1.Image = global::LAB6Soft.Properties.Resources._3961494681_a4fca1a1_5ef9_4440_93f7_764ea249e4f6;
+            this.pictureBox1.Location = new System.Drawing.Point(467, 87);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 112);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::LAB6Soft.Properties.Resources.kisspng_accounting_accountant_computer_icons_bookkeeping_f_finance_5abc92d44cb875_8221489015223077963143;
+            this.pictureBox5.Location = new System.Drawing.Point(56, 165);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 11;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::LAB6Soft.Properties.Resources.background_text;
+            this.pictureBox3.Location = new System.Drawing.Point(533, 292);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(254, 38);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 7;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::LAB6Soft.Properties.Resources.background_text;
+            this.pictureBox2.Location = new System.Drawing.Point(533, 390);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(254, 38);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
             // 
             // frmLogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(25)))), ((int)(((byte)(64)))));
+            this.BackColor = System.Drawing.Color.DarkSlateBlue;
             this.ClientSize = new System.Drawing.Size(1000, 600);
-            this.Controls.Add(this.pictureBox6);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.pictureBox5);
             this.Controls.Add(this.textBox2);
@@ -220,12 +210,11 @@
             this.Text = "UMiLDesk Software";
             this.Load += new System.EventHandler(this.frmLogin_Load);
             this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -244,6 +233,5 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.PictureBox pictureBox5;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox6;
     }
 }
